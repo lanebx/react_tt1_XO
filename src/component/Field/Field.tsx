33d5ff335585696534e1d3742fd3941da4d5f8cd
lanebx <havchikos@gmail.com>
2021-12-25 00:41:03 +0200

    added field and just blocks

--- src/component/Field/Field.tsx
+++ src/component/Field/Field.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/no-unused-state */
 import React from 'react';
 import './Field.scss';
 
 interface State {
-  firstPlayerName: string;
-  secondPlayerName: string;
+  fieldArray: number[];
 }
 
 interface Props {
@@ -14,14 +14,23 @@ interface Props {
 
 export class Field extends React.Component<Props, {}> {
   state: State = {
-    firstPlayerName: '',
-    secondPlayerName: '',
+    fieldArray: [0, 0, 0, 0, 0, 0, 0, 0, 0],
   };
 
   render() {
+    const { fieldArray } = this.state;
+
     return (
       <div className="Field">
-
+        <div className="Field-container">
+          {fieldArray.map((item: number, i: number) => {
+            return (
+              <div key={i} className="Field__block">
+                {item + 1}
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
